6bff6716c46a8e1aa1b9e94114797d89750f0b73
Manuel Bosi <manuelbosi57@gmail.com>
2021-02-27 12:27:57 +0100

    create orders table

---
+++ database/migrations/2021_02_27_112305_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table -> id();
+
+            $table -> string('order_code', 6) -> unique();
+            $table -> string('customer_name', 100);
+            $table -> string('customer_address', 200);
+            $table -> string('customer_phone', 200);
+            $table -> date('date');
+            $table -> string('status');
+            $table -> integer('total_price');
+
+            $table -> timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
