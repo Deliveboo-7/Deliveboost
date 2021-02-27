a40924c38af53f409f872a5d22de7666d9a22b0e
Manuel Bosi <manuelbosi57@gmail.com>
2021-02-27 12:22:13 +0100

    create dish_order table

---
+++ database/migrations/2021_02_27_112036_create_dish_order_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dish_order', function (Blueprint $table) {
+            $table -> id();
+
+            $table -> unsignedBigInteger('order_id');
+            $table -> unsignedBigInteger('dish_id');
+
+            $table -> timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dish_order');
+    }
+}
