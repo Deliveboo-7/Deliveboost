8e36da85d3c92ad0c74ec2f323f7f81a45733c4e
Manuel Bosi <manuelbosi57@gmail.com>
2021-02-27 12:35:03 +0100

    dishes belongsToMany orders

--- app/Dish.php
+++ app/Dish.php
@@ -17,4 +17,8 @@ class Dish extends Model
     public function user() {
         return $this -> belongsTo(User::class);
     }
+
+    public function orders() {
+        return $this -> belongsToMany(Order::class);
+    }
 }
