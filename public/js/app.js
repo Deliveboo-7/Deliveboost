ad1b51ebf896a80c4cc5f68b3ca17ce46890b28f
Anna Rosalen <rosalenanna@gmail.com>
2021-03-01 11:15:41 +0100

    ignore js

--- public/js/app.js
+++ public/js/app.js
@@ -19432,10 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-
-__webpack_require__(/*! C:\Laravel\Deliveboost\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Laravel\Deliveboost\resources\sass\app.scss */"./resources/sass/app.scss");
-
+__webpack_require__(/*! C:\progetti\Deliveroo\Deliveboost\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\progetti\Deliveroo\Deliveboost\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
