450c7dd4eda73af6dea2a5c3aa3c3dfab06f0aaf
OmarSpedalieri <omarspedalieri.vfx@gmail.com>
2021-03-02 10:35:53 +0100

    Refactoring

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -1,11 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
-use App\Dish;
+use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
@@ -13,40 +10,6 @@ class RestaurantController extends Controller
     {
         $this->middleware('auth');
     }
-
-    public function create(){
-
-        return view('dishes-create');
-    }
-
-
-    public function store(Request $request){
-    
-        $data = $request -> all();
-
-        $validatedData = $request -> validate([
-            'name' => 'required|string|min:5|max:50',
-            'desc' => 'required|string|min:20',
-            'price' => 'required',
-            'visible' => 'required',
-            ]);
-
-    
-        $data['price'] *= 100;
-
-        $loggedUser = Auth::user();
-        $newDish = Dish::make($data);
-        $newDish -> user() -> associate($loggedUser);
-        $newDish -> save();
-
-        return redirect() -> route('home');
-
-        
-    }
-
-
-
-
 }
 
 
