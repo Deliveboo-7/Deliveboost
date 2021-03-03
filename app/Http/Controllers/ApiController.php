3e23caf8507631df3127670bc749aa3de9d9dda4
Manuel Bosi <manuelbosi57@gmail.com>
2021-03-03 16:59:36 +0100

    create api to get restaurant by typology id

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Typology;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ApiController extends Controller
 {
+    public function restaurantsByTypology($id) {
+//        dd($typeName);
+//        Find typology clicked by user
+        $typology = Typology::findOrFail($id);
 
+//        Get all restaurants VERSION 1
+        $restaurants = $typology -> users() -> get();
+//        $typology = $restaurants -> typologies() -> get();
+
+//        Get all restaurants VERSION 2
+//        $restaurants = DB::table('typology_user') -> where('typology_id', $typology -> id) -> get();
+        return response() -> json($restaurants);
+    }
 }
