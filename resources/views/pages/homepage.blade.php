26d6a450440f125dea4cb7f3970a3134928a8aa8
LucaCavDev <71646753+LucaCavDev@users.noreply.github.com>
2021-03-03 17:11:40 +0100

    homepage

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -2,32 +2,46 @@
 
 @section('content')
 
-    <div class="homeContainer">
+    <div class="homeContainer container-fluid">
 
-        <section class="homeJumbotron">
+        <section class="homeJumbotron col-xs-12">
             <h1>DELIVEBOOST</h1>
             <h2>The luxuriest end fastest food delivery service</h2>
         </section>
 
-        <section  class="homeTypologies">
+        <section  class="homeTypologies col-xs-12">
 
-            <div class="homeTypo"><img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt=""></div>
-            <div class="homeTypo"><img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt=""></div>
-            <div class="homeTypo"><img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt=""></div>
-            <div class="homeTypo"><img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt=""></div>
-            <div class="homeTypo"><img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt=""></div>
-            <div class="homeTypo"><img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt=""></div>
-            <div class="homeTypo"><img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt=""></div>
-            <div class="homeTypo"><img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt=""></div>
+            @foreach ($typologies as $typology)
+                
+                <div 
+                class="homeTypo col-xs-12" 
+                @click="getData({{ $typology -> id }})"
+                >
+                    <img 
+                        src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt=""
+                        
+                    >
+                    <div class="homeTypName">
+                        <span>
+                            {{ $typology -> id }}
+                        </span>
+                    </div>
+                </div>
+                
+                
+            @endforeach
+
 
 
         </section>
 
-        <section  class="homeRestourants">
+        <section  class="homeRestourants  col-xs-12">
 
             <h2>
                 Select the types of food you prefer, and find the restaurants that have them all!
             </h2>
+
+
             
         </section>
 
