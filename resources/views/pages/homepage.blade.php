9a18627c38691bbb311640f32a8d82ceb41289c4
LucaCavDev <71646753+LucaCavDev@users.noreply.github.com>
2021-03-04 12:01:27 +0100

    homepage

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -2,11 +2,17 @@
 
 @section('content')
 
-    <div class="container-fluid homeContainer">
+    <div class="container-fluid homeContainer min-vh-100">
 
-        <section class="homeJumbotron row">
-            <h1 class="col-sm-10 offset-sm-1">DELIVEBO<img src="storage/icons/logo.svg"/>ST</h1>
-            <h2 class="col-sm-10 offset-sm-1">The luxuriest end fastest food delivery service</h2>
+        <section class="homeJumbotron row d-flex justify-content-center py-5 px-3    ">
+
+            <div class="d-flex justify-content-center col-lg-12">            
+                <h1 class="">DELIVEBO</h1>
+                <img  class="" src="{{ asset('storage/icons/logo.svg') }}"/>
+                <h1 class="">ST</h1>   
+            </div>
+
+            <h2 class="text-center col-lg-12">The luxuriest end fastest food delivery service</h2>
         </section>
 
         <section  class="homeTypologies row">
@@ -14,16 +20,17 @@
             @foreach ($typologies as $typology)
                 
                 <div 
-                class="homeTypo" 
+                class="homeTypo col-12 pb-3  col-sm-6 pb-sm-4 col-lg-3" 
+                
                 @click="getData({{ $typology -> id }})"
                 >
+                <!--         -->
+
                     <img 
                         src="https://d2e5ushqwiltxm.cloudfront.net/wp-content/uploads/sites/140/2020/07/16090452/A659763.jpg" alt=""                        
                     >
                     <div class="homeTypName">
-                        <span>
-                            {{ $typology -> id }}
-                        </span>
+                        {{ $typology -> name }}
                     </div>
                 </div>
                 
@@ -34,8 +41,8 @@
 
         </section>
 
-        <section  class="homeRestourants row">
-
+        <section  class="homeRestourants row ">
+<!--     py-5 px-3    -->
             <h2 class="col-sm-10 offset-sm-1">
                 Select the types of food you prefer, and find the restaurants that have them all!
             </h2>
