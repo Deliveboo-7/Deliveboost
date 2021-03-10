5854d1ee8cf3c671ff571ad26a79f6247cbc930a
OmarSpedalieri <omarspedalieri.vfx@gmail.com>
2021-03-10 08:44:42 +0100

    clean menu page and css

--- resources/views/pages/menu-index.blade.php
+++ resources/views/pages/menu-index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+{{-- -------------------------- COVER IMAGE AND TITLE ------------------------- --}}
+
     <div class="container-fluid containerPageMenu " id="cart-anna">
         <section class=" row no-gutters">
             <div class="coverRist col-12 d-flex flex-row align-items-center justify-content-center">
@@ -10,20 +12,26 @@
                 </div>
             </div>
         </section>
+    </div>
 
-        <section class="row no-gutters">
-            <div class="col-12 order-2 col-lg-7 order-lg-1 menuList accordion" id="accordion">
 
-                <ul>
-                    <li v-for="dish in dishes" class="text-gold" >
-                        [@{{ dish.id }}] @{{ dish.name }}  - @{{ dish.price/100 }} € 
-                        <i class="fas fa-plus" @click="addDish(dish)"></i>
-                    </li>
-                </ul>
+{{-- --------------------------RESTAURANT DISH LIST ------------------------- --}}
+
+<div class="container ">
+        <section class="row no-gutters ">
+
+            <div class="col-12 order-2 col-lg-6 order-lg-1 text-lighter ">
+
+                <div v-for="dish in dishes" class=" text-silver  my-3 px-2 d-flex justify-content-between align-items-center" style="font-size: 1.25em">
+                    @{{ dish.name }}-----------------------@{{ dish.price/100 }} €
+                <i class="fas fa-plus text-gold " @click="addDish(dish)"></i>
+
+            </div>
+
             </div>
 
             <!--SIDEBAR-->
-            <div class="col-12 order-1 col-lg-5 order-lg-2 container-sidebar">
+            <div class="col-12 order-1 col-lg-6 order-lg-2 px-4">
                 <div class="row no-gutters sidebar sticky-top p-2">
 
                     <div class="col-12 generalInfo pb-3 pt-3">
@@ -32,20 +40,20 @@
                             <!--left-->
                             <div class="col-6 px-2">
                                 <div class="col-12 detail">
-                                    <span class="detailTitle">Address:</span>
-                                    <span>{{ $restaurant -> address }}</span>
+                                    <span class="text-gold">Address:</span>
+                                    <span class="text-silver pr-3">{{ $restaurant -> address }}</span>
                                 </div>
                                 <div class="col-12 detail">
-                                    <span class="detailTitle">Website:</span>
-                                    <span>{{ $restaurant -> website }}</span>
+                                    <span class="text-gold">Website:</span>
+                                    <span class="text-silver v">{{ $restaurant -> website }}</span>
                                 </div>
                                 <div class="col-12 detail">
-                                    <span class="detailTitle">Phone:</span>
-                                    <span>{{ $restaurant -> phone_number }}</span>
+                                    <span class="text-gold">Phone:</span>
+                                    <span class="text-silver pr-3">{{ $restaurant -> phone_number }}</span>
                                 </div>
                                 <div class="col-12 detail">
-                                    <span class="detailTitle">Typology:</span>
-                                    <span>
+                                    <span class="text-gold">Typology:</span>
+                                    <span  class="text-silver pr-3">
                                         @foreach ($restaurant -> typologies as $typology)
                                             {{$typology -> name}}@if (!$loop->last), @endif
 
@@ -53,15 +61,15 @@
                                     </span> <!--da inserire-->
                                 </div>
                                 <div class="col-12 detail">
-                                    <span class="detailTitle">Delivery:</span>
-                                    <span>2,50€</span>
+                                    <span class="text-gold">Delivery:</span>
+                                    <span class="text-silver">2,50€</span>
                                 </div>
                                 <div class="col-12 detail">
-                                    <span class="detailTitle">Payment:</span>
-                                    <span><i class="fas fa-money-bill-wave"></i>
+                                    <span class="text-gold">Payment:</span>
+                                    <span class="text-silver"><i class="fas fa-money-bill-wave"></i>
                                         Cash
                                     </span>
-                                    <span>
+                                    <span class="text-silver">
                                         <i class="fa fa-credit-card" aria-hidden="true"></i>
                                         Credit Card
                                     </span>
@@ -71,8 +79,8 @@
                             <!--right-->
                             <div class="col-6 opening">
                                 <div class="col-12">
-                                    <span class="detailTitle">OPENING HOURS:</span>
-                                    <p>{{ $restaurant -> opening_info }}</p>
+                                    <span class="text-gold">OPENING HOURS:</span>
+                                    <p  class="text-silver">{{ $restaurant -> opening_info }}</p>
                                 </div>
                             </div>
 
@@ -80,24 +88,23 @@
                     </div> <!--fine generalInfo-->
 
                     <div class="col-12 cart p-4 " >
-                        <div class="dishesSelected d-none d-lg-block">
+                        <div class="dishesSelected d-none d-lg-block text-right">
                             <h4>YOUR ORDER</h4>
 
                             <div class="itemsOrdered mb-2">
 
                                 <ul v-if="cart.length === 0">
-                                    <li >
-                                        vuoto
+                                    <li class="text-silver  " >
+                                        Your cart is empty.
                                     </li>
                                 </ul>
 
                                 <ul v-else>
-                                   <li v-for="item in cart" class="d-flex justify-content-between pb-2">
-                                       
-                                       <span >[@{{ item.id }}] @{{ item.name }} --> @{{ item.quantity}}</span>
-                                       
-                                   </li>
-                               </ul>
+                                    <li v-for="item in cart" class="d-flex justify-content-between pb-2 text-silver">
+                                        <div class="inline"> @{{ item.name }}</div>
+                                        <div class="inline">Qty: @{{ item.quantity}}</div>
+                                    </li>
+                                </ul>
 
                             </div>
 
