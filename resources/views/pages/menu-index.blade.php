5625c0799479991f9716b9e8eb260061e13265ea
OmarSpedalieri <omarspedalieri.vfx@gmail.com>
2021-03-12 10:22:10 +0100

    menu-index bug

--- resources/views/pages/menu-index.blade.php
+++ resources/views/pages/menu-index.blade.php
@@ -40,8 +40,8 @@
             
             </div>
             
-            <div v-for="dish in dishes" v-show="dish.visible" class="row text-silver my-2 p-2 bord-lbr">
-                {{-- <div v-show="dish.visible"> --}}
+            <div v-for="dish in dishes"   class="row text-silver my-2 p-2 bord-lbr">
+             
                     <div class="col-7 " style="font-size: 1em;">
                         @{{ dish.name }}
                     </div>
@@ -64,7 +64,7 @@
                         </div>    
                     </div>
 
-                {{-- </div> --}}
+                
 
 
 
