06351639cfdf997c64da804c9c7eaa62ccb6b3b1
LucaCavDev <71646753+LucaCavDev@users.noreply.github.com>
2021-03-12 12:41:20 +0100

    proposta footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="footer text-center container-fluid ">
         <div class="row">
 
-            <div class=" col-md-2 offset-md-1">
+            {{-- <div class=" col-md-2 offset-md-1">
                 <a href="#">manuel git</a>
             </div>
 
@@ -19,8 +19,33 @@
             </div>
 
             <div class=" col-md-2 ">
-                <a href="#">luca git</a>
+                <a href="https://www.linkedin.com/in/luca-cavicchioli/">Luca Cavicchioli</a>
+            </div> --}}
+
+            <div class="col-6 col-md-3">
+                <div class="pb-2" href="">Manuel Bosi</div>
+                <a href="https://github.com/manuelbosi"><i class="fab fa-github-square fa-2x pr-1"></i></a>
+                <a href="https://www.linkedin.com/in/manuelbosi/"><i class="fab fa-linkedin fa-2x pl-1"></i></a>
             </div>
+
+            <div class="col-6 col-md-3 ">
+                <div class="pb-2" href="">Omar Spedalieri</div>
+                <a href="https://github.com/OmarSpedalieri"><i class="fab fa-github-square fa-2x pr-1"></i></a>
+                <a href="https://www.linkedin.com/in/omar-spedalieri-807a5a2a/"><i class="fab fa-linkedin fa-2x pl-1"></i></a>
+            </div>
+
+            <div class="col-6 col-md-3 ">
+                <div class="pb-2" href="">Anna Rosalen</div>
+                <a href="https://github.com/annarosalen"><i class="fab fa-github-square fa-2x pr-1"></i></a>
+                <a href="https://www.linkedin.com/in/anna-rosalen/"><i class="fab fa-linkedin fa-2x pl-1"></i></a>
+            </div>
+
+            <div class="col-6 col-md-3 ">
+                <div class="pb-2" href="">Luca Cavicchioli</div>
+                <a href="https://github.com/LucaCavDev"><i class="fab fa-github-square fa-2x pr-1"></i></a>
+                <a href="https://www.linkedin.com/in/luca-cavicchioli/"><i class="fab fa-linkedin fa-2x pl-1"></i></a>
+            </div>
+
 
         </div>
     </div>
