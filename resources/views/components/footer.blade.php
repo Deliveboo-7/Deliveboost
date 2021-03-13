199ead24a4476e052746564ff5ac21fa903bc368
LucaCavDev <71646753+LucaCavDev@users.noreply.github.com>
2021-03-13 15:06:46 +0100

    foooter

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -14,21 +14,13 @@
                 <a href="https://www.linkedin.com/in/omar-spedalieri-807a5a2a/" target="_blank"><i class="fab fa-linkedin fa-2x pl-1"></i></a>
             </div>
 
-<<<<<<< HEAD
-            <div class="col-6 col-md-3">
-=======
             <div class="col-6 col-md-3 mt-3 mt-md-0">
->>>>>>> 6f0955b9238a5d3599f838d9d46f2b49a51112c2
                 <div class="pb-2" href="">Anna Rosalen</div>
                 <a href="https://github.com/annarosalen" target="_blank"><i class="fab fa-github-square fa-2x pr-1"></i></a>
                 <a href="https://www.linkedin.com/in/anna-rosalen/" target="_blank"><i class="fab fa-linkedin fa-2x pl-1"></i></a>
             </div>
 
-<<<<<<< HEAD
-            <div class="col-6 col-md-3">
-=======
             <div class="col-6 col-md-3 mt-3 mt-md-0">
->>>>>>> 6f0955b9238a5d3599f838d9d46f2b49a51112c2
                 <div class="pb-2" href="">Luca Cavicchioli</div>
                 <a href="https://github.com/LucaCavDev" target="_blank"><i class="fab fa-github-square fa-2x pr-1"></i></a>
                 <a href="https://www.linkedin.com/in/luca-cavicchioli/" target="_blank"><i class="fab fa-linkedin fa-2x pl-1"></i></a>
